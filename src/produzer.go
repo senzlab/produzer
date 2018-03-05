d79ed9a02e34fe6945c169f1f6da9ac195aff497
eranga bandara <eranga.herath@pagero.com>
2018-03-05 13:57:53 +0530

    go routene

--- src/produzer.go
+++ src/produzer.go
@@ -2,9 +2,9 @@ package main
 
 import (
     "fmt"
-    //"time"
 	"log"
     "os"
+    "bufio"
 	"github.com/Shopify/sarama"
 )
 
@@ -18,41 +18,50 @@ func main() {
 	sarama.Logger = log.New(os.Stdout, "", log.Ltime)
     producer, err := initProducer()
     if err != nil {
-        fmt.Println("Error producer:", err.Error())
+        fmt.Println("Error producer: ", err.Error())
         os.Exit(1)
     }
 
-    // publish data
-    //producer.Input() <- &sarama.ProducerMessage{
-    //    Topic: topic,
-    //    Partition: 0,
-    //    Value: sarama.StringEncoder("helllll"),
-    //}
-    msg := &sarama.ProducerMessage {
-        Topic: "test",
-        Partition: 0,
-        Value: sarama.StringEncoder("era"),
-    }
-    p, o, err := producer.SendMessage(msg)
-    if err != nil {
-        fmt.Println("Error producer:", err.Error())
-    }
+    // read command line input
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print("Enter msg: ")
+        msg, _ := reader.ReadString('\n')
 
-    fmt.Println("partition :", p)
-    fmt.Println("offset :", o)
+        // publish
+        go publish(msg, producer)
+    }
 }
 
 func initProducer()(sarama.SyncProducer, error) {
 	config := sarama.NewConfig()
-    //config.Producer.Retry.Max = 5
+    config.Producer.Retry.Max = 5
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Return.Successes = true
+
+    // async producer
     //prd, err := sarama.NewAsyncProducer([]string{kafkaConn}, config)
+
+    // sync producer
     prd, err := sarama.NewSyncProducer([]string{kafkaConn}, config)
+
+    return prd, err
+}
+
+func publish(message string, producer sarama.SyncProducer) {
+    // publish sync
+    msg := &sarama.ProducerMessage {
+        Topic: topic,
+        Value: sarama.StringEncoder(message),
+    }
+    p, o, err := producer.SendMessage(msg)
     if err != nil {
-        fmt.Println("Error producer:", err.Error())
-        return nil, err
+        fmt.Println("Error publish: ", err.Error())
     }
 
-    return prd, nil
+    // publish async
+    //producer.Input() <- &sarama.ProducerMessage{
+
+    fmt.Println("Partition: ", p)
+    fmt.Println("Offset: ", o)
 }
